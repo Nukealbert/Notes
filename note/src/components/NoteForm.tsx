dd1685c5a033dbba708ba28cf2e2cc0361e3fc04
Kundan Kumar <39196236+Nukealbert@users.noreply.github.com>
2023-05-24 08:39:37 +0530

    Local storage is being used to store all the tags, title and body of note

--- note/src/components/NoteForm.tsx
+++ note/src/components/NoteForm.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import {Form,Stack,Row,Col} from 'react-bootstrap';
+import {useRef, useState} from 'react';
+import {Form,Stack,Row,Col,Button} from 'react-bootstrap';
+import CreatableReactSelect from 'react-select/creatable';
+import {Link} from 'react-router-dom';
+import { NoteData, Tag } from '../App';
 
-function NoteForm() {
+type NoteFormProps={
+    onSubmit: (data:NoteData)=> void
+    
+}
+
+function NoteForm({onSubmit}:NoteFormProps) {
+        const titleRef= useRef<HTMLInputElement>(null)
+        const bodyRef= useRef<HTMLTextAreaElement>(null)
+        const [selectedTags, setSelectedTags]=useState<Tag[]>([])
+
+        function handleSubmit(e: FormEvent){
+            e.preventDefault()
+            onSubmit({
+                title:titleRef.current!.value,
+                body:bodyRef.current!.value,
+                tags:[]
+            })
+        }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <Stack gap={4}>
             <Row>
                 <Col>
                     <Form.Group controlId='title'>
                         <Form.Label>Title</Form.Label>
-                        <Form.Control required />
+                        <Form.Control ref={titleRef} required />
 
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group controlId='tags'>
                         <Form.Label>Tags</Form.Label>
-                        <Form.Control required />
+                        <CreatableReactSelect value={selectedTags.map(tag=>{
+                            return {label:tag.label, value:tag.id}
+                        })} 
+                        onChange={tags=>{
+                            setSelectedTags(tags.map(tag=>{
+                                return{
+                                    label:tag.label,
+                                    id:tag.value
+                                }
+                            }))
+                        }}
+                        isMulti />
 
                     </Form.Group>
                 </Col>
             </Row>
+            <Form.Group controlId='markdown'>
+                <Form.Label>Body</Form.Label>
+                <Form.Control ref={bodyRef} required as={'textarea'} rows={15} />
+            </Form.Group>
+            <Stack direction='horizontal' gap={2} className='justify-content-end'>
+                <Button type='submit' >Save</Button>
+
+                <Link to='..' >
+                     <Button type='button' variant='outline-secondary'>Cancle</Button>
+                </Link>
+                
+            </Stack>
         </Stack>
 
     </Form>
